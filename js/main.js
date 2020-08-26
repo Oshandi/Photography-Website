457d2130ccd4bd897803e601fdcd455b104f3357
Piyumi Dissanayake <piyumi.dissanayake@ifsworld.com>
2020-08-26 13:04:25 +0530

    Filter gallery image categories

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-//menu toggle
+//Menu toggle
 $('.menutoggle').click(function() {
   $('menu').toggleClass('active');
   $(this).toggleClass('active');
 });
 
-//gallery layout
+//Gallery layout
 var $lightbox = $('#lightbox');
 
 $('[data-target="#lightbox"]').on('click', function(event) {
@@ -17,7 +17,52 @@ $('[data-target="#lightbox"]').on('click', function(event) {
   $lightbox.find('img').attr('alt', alt);
 });
 
-//
+//Filter gallery images
+  var $imgs = $('#gallery img');
+  var $category = $('#category');
+  var tagged = {};
+
+  $imgs.each(function() {
+    var img = this;
+    var tags = $(this).data('tags');
+
+    if (tags) {
+      tags.split(',').forEach(function(tagName) {
+        if (tagged[tagName] == null) {
+          tagged[tagName] = [];
+        }
+        tagged[tagName].push(img);
+      })
+    }
+  })
+
+  $('<li/>', {
+    text: 'All',
+    class: 'active',
+    click: function() {
+      $(this)
+        .addClass('active')
+        .siblings()
+        .removeClass('active');
+      $imgs.show();
+    }
+  }).appendTo($category);
+
+  $.each(tagged, function(tagName) {
+    $('<li/>', {
+      text: tagName,
+      click: function() {
+        $(this)
+          .addClass('active')
+          .siblings()
+          .removeClass('active');
+        $imgs
+          .hide()
+          .filter(tagged[tagName])
+          .show();
+      }
+    }).appendTo($category);
+  });
 
 
 });
