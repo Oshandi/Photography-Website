29591bef6b9aec28ac2565cd13babbbd452568db
Piyumi Dissanayake <piyumi.dissanayake@ifsworld.com>
2020-08-26 12:14:07 +0530

    Implemented gallery layout

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
+$(document).ready(function() {
 //menu toggle
 $('.menutoggle').click(function() {
   $('menu').toggleClass('active');
   $(this).toggleClass('active');
+});
+
+//gallery layout
+var $lightbox = $('#lightbox');
+
+$('[data-target="#lightbox"]').on('click', function(event) {
+  var $img = $(this).find('img'),
+      src = $img.attr('src'),
+      alt = $img.attr('alt');
+
+  $lightbox.find('img').attr('src', src);
+  $lightbox.find('img').attr('alt', alt);
+});
+
+//
+
+
 });
